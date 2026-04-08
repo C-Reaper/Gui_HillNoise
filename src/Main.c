25f23bb31341e2ae2d0f650bcc212741f2adbc14
codeleaded <gnu.c.reaper@gmail.com>
2026-04-08 15:44:14 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -5,6 +5,11 @@
 
 
 TransformedView tv;
+float freq = 0.1f;
+
+float RGA_New_P_F(float p){
+    return RGA_New_P(p);
+}
 
 void Setup(AlxWindow* w){
     tv = TransformedView_Make(
@@ -19,6 +24,8 @@ void Update(AlxWindow* w){
     TransformedView_HandlePanZoom(&tv,w->Strokes,GetMouse());
     const Vec2 m = TransformedView_ScreenWorldPos(&tv,GetMouse());
     
+    if(Stroke(ALX_KEY_UP).DOWN)     freq *= 1.01f;
+    if(Stroke(ALX_KEY_DOWN).DOWN)   freq *= 0.99f;
 
     Clear(BLACK);
 
@@ -28,11 +35,6 @@ void Update(AlxWindow* w){
     //    const float l = px;
     //    Rect_RenderXX(WINDOW_STD_ARGS,x,(1.0f - l) * GetHeight(),1,l * GetHeight(),WHITE);
     //}
-
-
-    const Vec2 tl = TransformedView_ScreenWorldPos(&tv,(Vec2){ 0.0f,0.0f });
-    const Vec2 br = TransformedView_ScreenWorldPos(&tv,(Vec2){ GetWidth(),GetHeight() });
-    const Vec2 sd = TransformedView_WorldScreenLength(&tv,(Vec2){ 1.0f,1.0f });
 
     //for(int x = 0;x<GetWidth();x++){
     //    const float wx = TransformedView_ScreenWorldX(&tv,x);
@@ -59,70 +61,87 @@ void Update(AlxWindow* w){
     //    Rect_RenderXX(WINDOW_STD_ARGS,x,(1.0f - l) * GetHeight(),1,l * GetHeight(),WHITE);
     //}
 
-    for(int y = 0;y<GetHeight();y++){
-        const float wy = TransformedView_ScreenWorldY(&tv,y);
-        const float ty = wy - floorf(wy);
-        
-        const float ypx0 = (float)floorf(wy) - 1.0f;
-        const float ypx1 = (float)floorf(wy);
-        const float ypx2 = (float)ceilf(wy);
-        const float ypx3 = (float)ceilf(wy) + 1.0f;
-        const float ypy0 = (float)RGA_New_P(ypx0);
-        const float ypy1 = (float)RGA_New_P(ypx1);
-        const float ypy2 = (float)RGA_New_P(ypx2);
-        const float ypy3 = (float)RGA_New_P(ypx3);
-        
-        const Vec2 outy = CRSpline_Calc(
-            (Vec2){ ypx0,ypy0 },
-            (Vec2){ ypx1,ypy1 },
-            (Vec2){ ypx2,ypy2 },
-            (Vec2){ ypx3,ypy3 },
-            ty
-        );
-
-        for(int x = 0;x<GetWidth();x++){
-            const float wx = TransformedView_ScreenWorldX(&tv,x);
-            const float tx = wx - floorf(wx);
-            
-            const float xpx0 = (float)floorf(wx) - 1.0f;
-            const float xpx1 = (float)floorf(wx);
-            const float xpx2 = (float)ceilf(wx);
-            const float xpx3 = (float)ceilf(wx) + 1.0f;
-            const float xpy0 = (float)RGA_New_P(xpx0);
-            const float xpy1 = (float)RGA_New_P(xpx1);
-            const float xpy2 = (float)RGA_New_P(xpx2);
-            const float xpy3 = (float)RGA_New_P(xpx3);
-            
-            const Vec2 outx = CRSpline_Calc(
-                (Vec2){ xpx0,xpy0 },
-                (Vec2){ xpx1,xpy1 },
-                (Vec2){ xpx2,xpy2 },
-                (Vec2){ xpx3,xpy3 },
-                tx
-            );
-
-            //const float t = (tx + ty) * 0.5f;
-            //const float st = 3.0f * t * t - 2.0f * t * t * t;
-            //const float l = ((1.0f - st) * outx.y + st * outy.y) * 0.5f;
-
-            //const float l = (outx.y + outy.y) * 0.5f;
-            const Pixel c = Pixel_toRGBA(l,l,l,1.0f);
-            w->Buffer[y * GetWidth() + x] = c;
-        }
-    }
-
-    //for(float y = tl.y;y<br.y;y+=1.0f){
-    //    for(float x = tl.x;x<br.x;x+=1.0f){
-    //        const float px = (float)RGA_New_P(x);
-    //        const float py = (float)RGA_New_P(y);
+    //for(int y = 0;y<GetHeight();y++){
+    //    const float wy = TransformedView_ScreenWorldY(&tv,y);
+    //    const float ty = wy - floorf(wy);
+    //    
+    //    const float ypx0 = (float)floorf(wy) - 1.0f;
+    //    const float ypx1 = (float)floorf(wy);
+    //    const float ypx2 = (float)ceilf(wy);
+    //    const float ypx3 = (float)ceilf(wy) + 1.0f;
+    //    const float ypy0 = (float)RGA_New_P(ypx0);
+    //    const float ypy1 = (float)RGA_New_P(ypx1);
+    //    const float ypy2 = (float)RGA_New_P(ypx2);
+    //    const float ypy3 = (float)RGA_New_P(ypx3);
+    //    
+    //    const Vec2 outy = CRSpline_Calc(
+    //        (Vec2){ ypx0,ypy0 },
+    //        (Vec2){ ypx1,ypy1 },
+    //        (Vec2){ ypx2,ypy2 },
+    //        (Vec2){ ypx3,ypy3 },
+    //        ty
+    //    );
     //
-    //        const float l = (px + py) * 0.5f;
-    //        const Pixel c = Pixel_toRGBA(l,l,l,1.0f);
+    //    for(int x = 0;x<GetWidth();x++){
+    //        const float wx = TransformedView_ScreenWorldX(&tv,x);
+    //        const float tx = wx - floorf(wx);
     //        
-    //        const Vec2 sp = TransformedView_WorldScreenPos(&tv,(Vec2){ x,y });
-    //        Rect_RenderXX(WINDOW_STD_ARGS,sp.x,sp.y,sd.x,sd.y,c);
+    //        const float xpx0 = (float)floorf(wx) - 1.0f;
+    //        const float xpx1 = (float)floorf(wx);
+    //        const float xpx2 = (float)ceilf(wx);
+    //        const float xpx3 = (float)ceilf(wx) + 1.0f;
+    //        const float xpy0 = (float)RGA_New_P(xpx0);
+    //        const float xpy1 = (float)RGA_New_P(xpx1);
+    //        const float xpy2 = (float)RGA_New_P(xpx2);
+    //        const float xpy3 = (float)RGA_New_P(xpx3);
+    //        
+    //        const Vec2 outx = CRSpline_Calc(
+    //            (Vec2){ xpx0,xpy0 },
+    //            (Vec2){ xpx1,xpy1 },
+    //            (Vec2){ xpx2,xpy2 },
+    //            (Vec2){ xpx3,xpy3 },
+    //            tx
+    //        );
+    //
+    //        //const float t = (tx + ty) * 0.5f;
+    //        //const float st = 3.0f * t * t - 2.0f * t * t * t;
+    //        //const float l = ((1.0f - st) * outx.y + st * outy.y) * 0.5f;
+    //
+    //        //const float l = (outx.y + outy.y) * 0.5f;
+    //        const Pixel c = Pixel_toRGBA(l,l,l,1.0f);
+    //        w->Buffer[y * GetWidth() + x] = c;
     //    }
     //}
+
+    const Vec2 tl = TransformedView_ScreenWorldPos(&tv,(Vec2){ 0.0f,0.0f });
+    const Vec2 br = TransformedView_ScreenWorldPos(&tv,(Vec2){ GetWidth(),GetHeight() });
+    const Vec2 sd = TransformedView_WorldScreenLength(&tv,(Vec2){ 1.0f,1.0f });
+
+    for(float y = floorf(tl.y);y<br.y;y+=1.0f){
+        for(float x = floorf(tl.x);x<br.x;x+=1.0f){
+            const float t = freq * y;
+
+            const Vec2 out0 = CRSpline_Calc_F(x,floorf(t) - 1.0f,   freq,RGA_New_P_F);
+            const Vec2 out1 = CRSpline_Calc_F(x,floorf(t),          freq,RGA_New_P_F);
+            const Vec2 out2 = CRSpline_Calc_F(x,ceilf(t),           freq,RGA_New_P_F);
+            const Vec2 out3 = CRSpline_Calc_F(x,ceilf(t) + 1.0f,    freq,RGA_New_P_F);
+    
+            const Vec2 out = CRSpline_Calc(
+                out0,
+                out1,
+                out2,
+                out3,
+                t - floor(t)
+            );
+
+            //const float l = F32_Sigmoid((out.x + out.y) * 0.5f);
+            const float l = F32_Sigmoid(5.0f * (out.y - 0.5f));
+            //const float l = F32_Clamp(out.y,0.0f,1.0f);
+            const Pixel c = Pixel_toRGBA(l,l,l,1.0f);
+            const Vec2 sp = TransformedView_WorldScreenPos(&tv,(Vec2){ x,y });
+            Rect_RenderXX(WINDOW_STD_ARGS,sp.x,sp.y,sd.x + 1.0f,sd.y + 1.0f,c);
+        }
+    }
 
     //for(int y = 0;y<GetHeight();y++){
     //    for(int x = 0;x<GetWidth();x++){
@@ -135,6 +154,8 @@ void Update(AlxWindow* w){
     //        w->Buffer[y * GetWidth() + x] = c;
     //    }
     //}
+
+    CStr_RenderAlxFontf(WINDOW_STD_ARGS,GetAlxFont(),0.0f,0.0f,RED,"F: %f",freq);
 }
 void Delete(AlxWindow* w){
     
